57916f6688d6755ecf7d6e18fdf572e891d47ea7
scarlettwitch123 <134487761+scarlettwitch123@users.noreply.github.com>
2023-05-25 12:30:13 +0530

    First Commit

---
+++ DemoGit/src/test/java/com/helloWorld/DemoGit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloWorld.DemoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
